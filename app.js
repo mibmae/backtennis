79715a1b3ee8cf9e580aed64c1ad61d0e1d11529
BertrandPeltier <bertrandpeltier1979@gmail.com>
2021-11-11 11:50:08 +0100

    Base Express + index Router

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+
+const routers = require('./app/routers');
+
+const app = express();
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.json());
+app.use(routers);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
---
+++ app/routers/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const baseUrl = process.env.BASE_URL;
+
+router.get(`${baseUrl}/`, (_, response) => {
+    response.status(200).json({ message: 'Welcome to my API' });
+});
+
+router.use(errorsMiddleware.error404);
+router.use(errorsMiddleware.error500);
+
+module.exports = router;
