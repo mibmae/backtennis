3f21a5acfcd997804f0d28329a1b517e2a783139
BertrandPeltier <bertrandpeltier1979@gmail.com>
2021-11-11 12:26:48 +0100

    Errors middleware

---
+++ app/middlewares/errorsMiddleware.js
@@ -0,0 +1,20 @@
+module.exports = {
+    async error404(request, response) {
+        response.status(404).json({
+            error: {
+                message: "Ressource not found",
+                url: request.url,
+                method: request.method
+            }
+        })
+    },
+    async error500(error, _, response, __) {
+        response.status(500).json({
+            error: {
+                message: "Server error",
+                messageDetail: error.message,
+                infos: error
+            }
+        })
+    },
+};
--- app/routers/index.js
+++ app/routers/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const errorsMiddleware = require('../middlewares/errorsMiddleware');
 const router = express.Router();
 const baseUrl = process.env.BASE_URL;
 
 router.get(`${baseUrl}/`, (_, response) => {
-    response.status(200).json({ message: 'Welcome to my API' });
+    response.status(200).json({
+        message: 'Welcome to my API'
+    });
 });
 
 router.use(errorsMiddleware.error404);
