a3f5b764e823f6a0870efe2d78a407a37a3eaebc
BertrandPeltier <bertrandpeltier1979@gmail.com>
2021-11-14 12:41:25 +0100

    Ajout d'un commentaire pour tester

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(routers);
 
+// Lancement du server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
