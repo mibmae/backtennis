1d56a52b9dfdc40d05296d7c6a586ec5513bfe28
BertrandPeltier <bertrandpeltier1979@gmail.com>
2021-11-17 13:19:46 +0100

    Ajout de commentaire dans le dataMapper

--- app/dataMappers/index.js
+++ app/dataMappers/index.js
@@ -3,21 +3,37 @@ const client = require('./client');
 module.exports = {
     async findAll() {
 
-        // ** Simulation :
+        // TODO MySQL
         // const data = await client.query(`
         //     SELECT * FROM xxx
         // `);
+        // return data[0]
 
+        // TODO PostgresQL
+        // const data = await client.query(`
+        //     SELECT * FROM xxx
+        // `);
+        // return data.rows;
+
+        // ** Simulation avec de la fake data
         const data = [...client];
         return data;
     },
     async findById(id) {
 
-        // ** Simulation
+        // TODO MySQL
         // const data = await client.query(`
         //     SELECT * FROM xxx WHERE id = ?
         // `, [ id ]);
+        // return data[0][0];
 
+        // TODO PostgresQL
+        // const data = await client.query(`
+        //     SELECT * FROM xxx WHERE id = $1
+        // `, [ id ]);
+        // return data.rows[0];
+        
+        // ** Simulation avec de la fake data
         const data = client.filter(item => item.id == id);
         return data;
     },
