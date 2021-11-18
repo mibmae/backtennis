ed07f0be5cb850e3e2c15b5549916f536bd15212
BertrandPeltier <bertrandpeltier1979@gmail.com>
2021-11-18 13:47:46 +0100

    Hash des pwd dans les fakeData

--- app/fakeData/fakeData.js
+++ app/fakeData/fakeData.js
@@ -3,13 +3,13 @@ const fakeData = [
         id: 1,
         name: 'Bob',
         email: 'bob@gmail.com',
-        password: 'azerty',
+        password: '$argon2i$v=19$m=4096,t=3,p=1$8Q+G1QLg2xS2oJaIfLRsJw$zubs+qFwqmwbYwypD/fKlHS/402eG244/1BzuibToa4', // Azerty47!
     },
     {
         id: 2,
         name: 'John',
         email: 'john@gmail.com',
-        password: 'azerty',
+        password: '$argon2i$v=19$m=4096,t=3,p=1$8Q+G1QLg2xS2oJaIfLRsJw$zubs+qFwqmwbYwypD/fKlHS/402eG244/1BzuibToa4', // Azerty47!
     },
 ];
 
