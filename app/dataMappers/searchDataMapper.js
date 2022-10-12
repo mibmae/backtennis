d2988d1bc48c238b836144e7d58dae83c35daf61
mibmae <mibmae@gmail.com>
2022-10-12 15:02:18 +0200

    ajout de la recherche

--- app/dataMappers/searchDataMapper.js
+++ app/dataMappers/searchDataMapper.js
@@ -3,7 +3,7 @@ const client = require('./client');
 module.exports = {
     async find(text) {
         const data = await client.query(`
-            SELECT * FROM articles where titre ILIKE $1 ORDER by id DESC
+            SELECT * FROM articles where titre ILIKE $1 AND enligne = 'true' ORDER by id DESC
         `, ['%' + text + '%']);
         return data.rows;
     },
